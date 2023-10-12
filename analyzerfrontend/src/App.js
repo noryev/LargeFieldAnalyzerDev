cec5c78126a2e8e038817a6e4c4d764699b7e2cd
Logan Lentz <logan.lentz1@gmail.com>
2023-10-12 02:07:32 -0500

    handle upload function error messaging
    line to parse response body to js

--- analyzerfrontend/src/App.js
+++ analyzerfrontend/src/App.js
@@ -7,27 +7,31 @@ function App() {
   const handleUpload = async () => {
     const file = fileInput.current.files[0];
     if (!file) return;
-
+  
     const formData = new FormData();
     formData.append('file', file);
-
+  
     try {
       const response = await fetch('https://analyzer-backend.deanlaughing.workers.dev', {
         method: 'POST',
         body: formData,
         mode: 'cors'
       });
-
+  
+      const responseData = await response.json(); // Assuming the server responds with JSON. If it's plain text, use response.text().
+  
       if (response.ok) {
         alert('File uploaded successfully!');
       } else {
-        alert('Failed to upload file.');
+        console.error("Server Response:", responseData);
+        alert(`Failed to upload file. Reason: ${responseData.message || 'Unknown error'}`);
       }
     } catch (error) {
-      console.error("There was an error uploading the file.", error);
+      console.error("There was an error uploading the file:", error);
       alert('An error occurred. Please try again.');
     }
   };
+  
 
   return (
     <div className="App">
