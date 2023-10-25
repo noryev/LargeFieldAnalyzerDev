735b27419c8fe86c5e7d8365810e76dfd42348a9
noryev <logan.lentz1@gmail.com>
2023-10-25 03:55:00 -0500

    Dev Mode Working

--- analyzerbackend/worker.js
+++ analyzerbackend/worker.js
@@ -4,7 +4,7 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
   try {
-    const originHeader = request.headers.get('Origin') || 'https://divide-perennial-vqus8gw.dappling.network';
+    const originHeader = request.headers.get('Origin') || 'largefielddataanalyzer-9vu63o.dappling.network';
 
     if (request.method === 'OPTIONS') {
       return handleCORS(request, originHeader);
@@ -13,31 +13,65 @@ async function handleRequest(request) {
       const ipfsCID = requestData.ipfsCID;
 
       if (!ipfsCID) {
-        return new Response('IPFS hash not provided.', {
+        return new Response(JSON.stringify({ error: 'IPFS hash not provided.' }), {
           status: 400,
-          headers: { 'Access-Control-Allow-Origin': originHeader }
+          headers: { 'Access-Control-Allow-Origin': originHeader, 'Content-Type': 'application/json' }
         });
       }
 
       // Accessing the secrets
-      // ... (rest of your code remains the same)
+      const dataApiUrl = `https://us-east-2.aws.data.mongodb-api.com/app/data-uucwm/endpoint/data/v1`;
+      const clusterName = DATA_SOURCE_NAME;
+      const databaseName = DATABASE_NAME;
+      const collectionName = COLLECTION_NAME;
+      const dataApiKey = API_KEY;
 
-      return new Response('IPFS CID and other data saved successfully!', {
+      const documentToInsert = {
+        ipfsCID: ipfsCID,
+        // ... any other fields you want to insert ...
+      };
+
+      const responseFromMongoDB = await fetch(`${dataApiUrl}/action/insertOne`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Request-Headers': '*',
+          'api-key': dataApiKey,
+        },
+        body: JSON.stringify({
+          collection: collectionName,
+          database: databaseName,
+          dataSource: clusterName,
+          document: documentToInsert,
+        }),
+      });
+
+      if (!responseFromMongoDB.ok) {
+        const errorMessage = await responseFromMongoDB.text();
+        console.error('Failed to insert document into MongoDB Atlas:', errorMessage);
+        return new Response(JSON.stringify({ error: 'Failed to process the request.' }), {
+          status: 500,
+          headers: { 'Access-Control-Allow-Origin': originHeader, 'Content-Type': 'application/json' }
+        });
+      }
+
+      // If you reach this point, the document has been successfully inserted
+      return new Response(JSON.stringify({ message: 'IPFS CID and other data saved successfully!' }), {
         status: 200,
-        headers: { 'Access-Control-Allow-Origin': originHeader }
+        headers: { 'Access-Control-Allow-Origin': originHeader, 'Content-Type': 'application/json' }
       });
 
     } else {
-      return new Response('Please send a POST request.', {
+      return new Response(JSON.stringify({ error: 'Please send a POST request.' }), {
         status: 400,
-        headers: { 'Access-Control-Allow-Origin': originHeader }
+        headers: { 'Access-Control-Allow-Origin': originHeader, 'Content-Type': 'application/json' }
       });
     }
   } catch (error) {
     console.error(error);
-    return new Response('Failed to process the request.', {
+    return new Response(JSON.stringify({ error: 'Failed to process the request.' }), {
       status: 500,
-      headers: { 'Access-Control-Allow-Origin': originHeader }
+      headers: { 'Access-Control-Allow-Origin': originHeader, 'Content-Type': 'application/json' }
     });
   }
 }
@@ -57,6 +91,6 @@ function handleCORS(request, origin) {
     }
     return new Response(null, { headers: respHeaders });
   } else {
-    return new Response('CORS header check failed', { status: 400 });
+    return new Response(JSON.stringify({ error: 'CORS header check failed' }), { status: 400, headers: { 'Content-Type': 'application/json' } });
   }
 }
