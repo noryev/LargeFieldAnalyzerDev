63877c558a1a606f832507c169079ee0d8c21358
noryev <logan.lentz1@gmail.com>
2023-10-25 01:46:44 -0500

    bunches of updates

--- analyzerfrontend/src/App.js
+++ analyzerfrontend/src/App.js
@@ -2,30 +2,32 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [ipfsHash, setIpfsHash] = useState('');
+  const [ipfsCID, setIpfsCID] = useState('');
 
   const handleUpload = async () => {
-    if (!ipfsHash.trim()) return alert('Please enter an IPFS hash.');
+    if (!ipfsCID.trim()) return alert('Please enter an IPFS CID.');
+
+    const formData = new FormData();
+    formData.append('ipfsCID', ipfsCID);
 
     try {
-      const response = await fetch('https://large-field-analyzer.deanlaughing.workers.dev/upload', {
+      const response = await fetch('https://divide-perennial-vqus8gw.dappling.network/your-worker-route', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ ipfsHash }),
+        body: formData,
         mode: 'cors'
       });
 
       const responseText = await response.text();
 
       if (response.ok) {
-        alert('IPFS hash submitted successfully!');
+        alert('IPFS CID submitted successfully!');
         console.log('Server Response:', responseText);
       } else {
         console.error("Server Response:", responseText);
-        alert(`Failed to submit IPFS hash. Reason: ${responseText || 'Unknown error'}`);
+        alert(`Failed to submit IPFS CID. Reason: ${responseText || 'Unknown error'}`);
       }
     } catch (error) {
-      console.error("There was an error submitting the IPFS hash:", error);
+      console.error("There was an error submitting the IPFS CID:", error);
       alert('An error occurred. Please try again.');
     }
   };
@@ -37,8 +39,8 @@ function App() {
         <div>
           <input
             type="text"
-            value={ipfsHash}
-            onChange={(e) => setIpfsHash(e.target.value)}
+            value={ipfsCID}
+            onChange={(e) => setIpfsCID(e.target.value)}
             placeholder="Enter IPFS CID"
           />
           <button onClick={handleUpload}>Submit</button>
