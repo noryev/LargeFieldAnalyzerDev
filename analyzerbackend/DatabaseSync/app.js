60c04e646cac033f6d819347cd06d2609fd66e6d
noryev <logan.lentz1@gmail.com>
2023-10-25 03:22:56 -0500

    download IPFS data in sandbox

--- analyzerbackend/DatabaseSync/app.js
+++ analyzerbackend/DatabaseSync/app.js
@@ -1,49 +1,40 @@
 const { MongoClient } = require('mongodb');
 const IPFS = require('ipfs-http-client');
 
-async function main() {
-    const uri = "<Your MongoDB Connection String>";
+async function downloadFromIPFS() {
+    const uri = 'your_mongo_connection_string';
+    const dbName = 'your_database_name';
+    const collectionName = 'your_collection_name';
+
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const ipfs = IPFS.create(); // Connecting to IPFS on the default API address http://localhost:5001
 
     try {
-        // Connect to the MongoDB client
         await client.connect();
+        console.log('Connected to MongoDB');
 
-        // Connect to the IPFS node
-        const ipfs = IPFS.create({ host: 'localhost', port: '5001', protocol: 'http' });
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
 
-        // Get the CIDs from MongoDB
-        const cids = await getCIDs(client);
-
-        // Download each file from IPFS
-        for (const cid of cids) {
-            try {
+        const cursor = collection.find({});
+        await cursor.forEach(async (doc) => {
+            if (doc.ipfsCID) {
+                console.log(`Downloading content for CID: ${doc.ipfsCID}`);
                 const chunks = [];
-                for await (const chunk of ipfs.cat(cid)) {
+                for await (const chunk of ipfs.cat(doc.ipfsCID)) {
                     chunks.push(chunk);
                 }
-                const fileContent = Buffer.concat(chunks);
-                console.log(`File content of CID ${cid}:`, fileContent.toString());
-
-                // Here you can decide what you want to do with `fileContent`
-                // For example, save it to disk, process it, etc.
-
-            } catch (error) {
-                console.error(`Failed to download file with CID ${cid}:`, error.message);
+                const content = Buffer.concat(chunks);
+                require('fs').writeFileSync(`./downloads/${doc.ipfsCID}.txt`, content);
+                console.log('File downloaded and saved to disk');
             }
-        }
-
+        });
+    } catch (err) {
+        console.error(err);
     } finally {
         await client.close();
+        console.log('MongoDB connection closed');
     }
 }
 
-async function getCIDs(client) {
-    const database = client.db('yourDatabaseName');
-    const collection = database.collection('yourCollectionName');
-
-    const documents = await collection.find({}).toArray();
-    return documents.map(doc => doc.ipfsCID);
-}
-
-main().catch(console.error);
+downloadFromIPFS();
