ac77508b858e75c6cbb158f35beaa123de66555e
noryev <logan.lentz1@gmail.com>
2023-10-26 03:19:41 -0500

    pain

--- analyzerbackend/DatabaseSync/databasePull.js
+++ analyzerbackend/DatabaseSync/databasePull.js
@@ -9,7 +9,7 @@ dotenv.config();
 const uri = process.env.MONGO_URI;
 const dbName = process.env.DB_NAME;
 const collectionName = process.env.COLLECTION_NAME;
-const downloadsDir = './analyzerbackend/DatabaseSync/downloads';
+const downloadsDir = './downloads';
 
 if (!uri || !dbName || !collectionName) {
     console.error('One or more required environment variables are not set');
