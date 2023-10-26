a34110a4f5e51bc3e17294cec5d9cd7092a427a7
noryev <logan.lentz1@gmail.com>
2023-10-25 23:55:05 -0500

    directory for downloads added

--- analyzerbackend/DatabaseSync/app.js
+++ analyzerbackend/DatabaseSync/app.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const { create } = require('helia');
-const fs = require('fs');
+const { create } = require('ipfs-http-client'); // Updated import
+const fs = require('fs').promises; // Updated import
+const path = require('path');
 
 async function downloadFromIPFS() {
     const uri = process.env.MONGO_URI;
     const dbName = process.env.DB_NAME;
     const collectionName = process.env.COLLECTION_NAME;
+    const downloadsDir = './downloads';
 
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     const ipfs = create('/ip4/127.0.0.1/tcp/5001'); // Adjust the IPFS API address if necessary
@@ -19,18 +21,22 @@ async function downloadFromIPFS() {
         const collection = db.collection(collectionName);
 
         const cursor = collection.find({});
-        await cursor.forEach(async (doc) => {
+
+        await fs.mkdir(downloadsDir, { recursive: true });
+
+        for await (const doc of cursor) {
             if (doc.ipfsCID) {
                 console.log(`Downloading content for CID: ${doc.ipfsCID}`);
                 try {
                     const content = await ipfs.cat(doc.ipfsCID);
-                    fs.writeFileSync(`./downloads/${doc.ipfsCID}.txt`, content);
-                    console.log('File downloaded and saved to disk');
+                    const filePath = path.join(downloadsDir, `${doc.ipfsCID}.txt`);
+                    await fs.writeFile(filePath, content);
+                    console.log(`File downloaded and saved to ${filePath}`);
                 } catch (error) {
                     console.error(`Error downloading CID ${doc.ipfsCID}:`, error);
                 }
             }
-        });
+        }
     } catch (err) {
         console.error(err);
     } finally {
