852ef47eed399beebe60a86855e317b56b20cfee
Logan Lentz <logan.lentz1@gmail.com>
2023-10-30 04:39:52 -0500

    node modules ignore attempted

--- analyzerfrontend/src/App.js
+++ analyzerfrontend/src/App.js
@@ -1,5 +1,69 @@
 import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Footer() {
+  const handlePlaceholderClick = () => {
+    alert('This is a placeholder button.');
+  };
+
+  return (
+    <footer id="footer" className="footer">
+      <Container>
+        <Row className="gy-4">
+          <Col lg={5} md={12} className="footer-info">
+            <a href="index.html" className="logo d-flex align-items-center">
+              <span>Leto.gg</span>
+            </a>
+            <p>An Open-Source Community Operated Metrics Engine built for the Open-Web and its users.</p>
+            <div className="social-links d-flex mt-4">
+              <a href="https://twitter.com/LetoDev" className="twitter">
+                <i className="bi bi-twitter"></i>
+              </a>
+            </div>
+          </Col>
+
+          <Col lg={2} xs={6} className="footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><button type="button" onClick={handlePlaceholderClick} className="link-button">Home</button></li>
+              <li><button type="button" onClick={handlePlaceholderClick} className="link-button">About us</button></li>
+              <li><button type="button" onClick={handlePlaceholderClick} className="link-button">Terms of Service</button></li>
+              <li><button type="button" onClick={handlePlaceholderClick} className="link-button">Privacy Policy</button></li>
+            </ul>
+          </Col>
+
+          <Col lg={2} xs={6} className="footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><a href="https://github.com/Leto-gg/lego.gg-api">Metrics API's</a></li>
+              <li><button type="button" onClick={handlePlaceholderClick} className="link-button">Architecture</button></li>
+              <li><a href="https://github.com/Leto-gg">Github Repo</a></li>
+              <li>
+                <a href="https://chrome.google.com/webstore/detail/ipfs-stat-viewer/leoogniilogpecgamlbafoajfcaoddja">
+                  IPFS Stat Viewer
+                </a>
+              </li>
+            </ul>
+          </Col>
+
+          <Col lg={3} md={12} className="footer-contact text-center text-md-start">
+            <h4>Contact Us</h4>
+            <p>
+              admin@galaxyx.io<br />
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="mt-4">
+        <div className="credits">
+          {/* Add any necessary credits or additional footer content here */}
+        </div>
+      </Container>
+    </footer>
+  );
+}
 
 function App() {
   const [ipfsCID, setIpfsCID] = useState('');
@@ -49,6 +113,7 @@ function App() {
           <button onClick={handleUpload}>Submit</button>
         </div>
       </header>
+      <Footer />
     </div>
   );
 }
