723731cde6380e9d11b1a6f7c7c6f01c6f0b2707
noryev <logan.lentz1@gmail.com>
2023-11-17 06:47:37 -0600

    working maybe?

--- backend/DatabaseSync/dbScorePush.js
+++ backend/DatabaseSync/dbScorePush.js
@@ -1,4 +1,4 @@
-import { MongoClient, ObjectId } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
@@ -23,43 +23,66 @@ async function extractProperty(directory, fileName, propertyPath) {
         for (const key of propertyPath) {
             property = property[key];
             if (property === undefined) {
-                return null;
+                throw new Error(`Property ${propertyPath.join('.')} not found`);
             }
         }
         return property;
     } catch (err) {
-        console.error(`Error in ${path.basename(directory)}:`, err.message);
-        return null;
+        throw new Error(`Error reading ${fileName} in ${path.basename(directory)}: ${err.message}`);
     }
 }
 
 // Function to iterate over analysis directories and update database
 async function extractDataAndUpdateDatabase() {
+    let processedCount = 0;
+    let updateSuccessCount = 0;
+    let errorCount = 0;
+
     try {
         await client.connect();
         const database = client.db(dbName);
         const collection = database.collection(collectionName);
 
         const directories = await fs.promises.readdir(baseDir);
+        console.log(`Found ${directories.length} directories to process.`);
 
         for (const dir of directories) {
+            console.log(`Processing directory ${dir}...`);
             const directoryPath = path.join(baseDir, dir);
             const stat = await fs.promises.stat(directoryPath);
 
-            if (stat.isDirectory() && /^[0-9a-fA-F]{24}$/.test(dir)) {
-                const score = await extractProperty(directoryPath, 'reports/report.json', ['info', 'score']);
-                const target = await extractProperty(directoryPath, 'task.json', ['target']);
+            if (stat.isDirectory()) {
+                processedCount++;
+                try {
+                    const score = await extractProperty(directoryPath, 'reports/report.json', ['info', 'score']);
+                    const target = await extractProperty(directoryPath, 'task.json', ['target']);
 
-                const result = await collection.updateOne(
-                    { _id: new ObjectId(dir) },
-                    { $set: { score: score, target: target } }
-                );
+                    console.log(`Updating document with target ${target} with score: ${score}`);
 
-                console.log(`Updated document with id ${dir}:`, result);
+                    const result = await collection.updateOne(
+                        { ipfsCID: target },
+                        { $set: { score: score } }
+                    );
+
+                    if (result.modifiedCount > 0) {
+                        updateSuccessCount++;
+                        console.log(`Successfully updated document with target ${target}.`);
+                    } else {
+                        console.log(`No changes made to document with target ${target}.`);
+                    }
+                } catch (err) {
+                    errorCount++;
+                    console.error(`Error processing directory ${dir}:`, err.message);
+                }
+            } else {
+                console.log(`Skipping non-directory ${dir}.`);
             }
         }
+
+        console.log(`Process Summary: Processed ${processedCount} directories. Successfully updated ${updateSuccessCount} documents. Encountered errors in ${errorCount} directories.`);
+
     } catch (err) {
-        console.error('Error:', err.message);
+        console.error('Error during database connection or operation:', err.message);
     } finally {
         await client.close();
     }
