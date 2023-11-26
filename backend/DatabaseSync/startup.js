b955cbab86a7cec966e4a8fb2101d524cb1bff2f
noryev <logan.lentz1@gmail.com>
2023-11-25 20:53:07 -0600

    some error handlng

--- backend/DatabaseSync/startup.js
+++ backend/DatabaseSync/startup.js
@@ -1,7 +1,11 @@
 import { spawn } from 'child_process';
 
 function runInNewTerminal(command, args) {
-    spawn('gnome-terminal', ['--', 'bash', '-c', `${command} ${args.join(' ')}; exec bash`]);
+    const process = spawn('gnome-terminal', ['--', 'bash', '-c', `${command} ${args.join(' ')}; exec bash`]);
+
+    process.on('error', (err) => {
+        console.error(`Failed to start process: ${err.message}`);
+    });
 }
 
 runInNewTerminal('ipfs', ['daemon']);
